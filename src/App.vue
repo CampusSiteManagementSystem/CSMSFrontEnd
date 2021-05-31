e7482290b2ad3714170b9eae98e79db944b3f19a
李俊杰 <1061455426@qq.com>
2021-05-31 15:27:53 +0800

    Revert "Revert "IdentitySelection Page""
    This reverts commit fc7db312b0505e64459551fa780a5d54612c8618.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
