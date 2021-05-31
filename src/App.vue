fc7db312b0505e64459551fa780a5d54612c8618
李俊杰 <1061455426@qq.com>
2021-05-31 15:26:58 +0800

    Revert "IdentitySelection Page"
    This reverts commit aa805d16c174a1eafb0952f897430fc78aa0a1ef.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <router-view></router-view>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-
+  name: 'App',
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import './plugins/element.js'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
