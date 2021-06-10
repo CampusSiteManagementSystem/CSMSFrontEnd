edc2f2c0ebf36d3b93d552b0d53ec2944b52ea29
Zzy <74710014+Zhaobaba918@users.noreply.github.com>
2021-06-10 23:17:45 +0800

    和罗老师合并

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -18,7 +18,7 @@
         <i class="el-icon-house"></i>
         <span slot="title">主页</span>
       </el-menu-item>
-      <el-menu-item index="">
+      <el-menu-item index="/Groundsman/ActivityList">
         <i class="el-icon-finished"></i>
         <span slot="title">审核活动记录</span>
       </el-menu-item>
@@ -38,7 +38,7 @@
         <i class="el-icon-chat-dot-square"></i>
         <span slot="title">查看反馈</span>
       </el-menu-item>
-      <el-menu-item index="/groundinfo">
+      <el-menu-item index="/Groundsman/GroundList">
         <i class="el-icon-edit-outline"></i>
         <span slot="title">维护场地信息</span>
       </el-menu-item>
--- src/router/index.js
+++ src/router/index.js
@@ -73,6 +73,43 @@ const routes = [{
 //         // import ('../components/personinfocard')
 //          import ('../page/testpage3')
 // },
+
+
+
+//罗
+{
+    path: '/SystemAdmin/PublishSystemAnnouncement',
+    name: 'Publish',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../page/Publish.vue')
+  },
+  {
+    path: '/Groundsman/ActivityList',
+    name: 'ActivityList',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../page/ActivityList.vue')
+  },
+  {
+    path: '/Groundsman/GroundList',
+    name: 'GroundList',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../page/GroundList.vue')
+  },
+  {
+    path: '/Groundsman/ActivityInfo',
+    name: 'ActivityInfo',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../page/ActivityInfo.vue')
+  }
+
 ]
 
 const router = new VueRouter({
