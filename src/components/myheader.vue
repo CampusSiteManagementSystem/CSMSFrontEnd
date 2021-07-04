743cbba40f654674a5ee53dbf090bb21f06f7247
Zzy <74710014+Zhaobaba918@users.noreply.github.com>
2021-07-04 12:37:05 +0800

    Delete src directory

--- src/components/myheader.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <el-row class="headerrow" type="flex" justify="end" align="middle" >
-        <el-button type="text" ><el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar></el-button>
-        <el-button type="text" >王二狗</el-button>
-      </el-row>
-</template>
