e90bb425cfbb8ce90a31f66f038b27d99ff3e508
yang yuqing <957890361@qq.com>
2021-07-06 12:40:14 +0800

    update layout

--- src/page/SystemAdmin/SystemAdminGroupVerify.vue
+++ src/page/SystemAdmin/SystemAdminGroupVerify.vue
@@ -3,57 +3,47 @@
     <div>
       <h2>审批组织注册</h2>
     </div>
-    <el-row>
-      <el-col :span="20"
-        ><div class="info">
-          <br />
-          <p>组织信息:</p>
-          <br />
-          <p>组织名称:</p>
-          <br />
-          <p>详细信息:</p>
-          <br /></div></el-col
-    ></el-row>
+    <el-card>
+      <el-row>
+        <el-col :span="20"
+          ><div class="info"> 
+            <p>组织信息:</p>         
+            <p>组织名称:</p>          
+            <p>详细信息:</p>
+            </div></el-col
+      ></el-row>
 
-    <el-row>
-      <el-col :span="20"
-        ><div class="status">
-          <p>审核意见</p>
-          <el-radio v-model="radio" label="1">通过</el-radio>
-          <el-radio v-model="radio" label="2">不通过</el-radio>
+      <el-row>
+        <el-col :span="20"
+          ><div class="status">
+            <p>审核意见</p>
+            <el-radio v-model="radio" label="1">通过</el-radio>
+            <el-radio v-model="radio" label="2">不通过</el-radio>
 
-          <br />
-          <p>审核说明</p>
-          <el-input
-            type="textarea"
-            :rows="5"
-            placeholder="请输入内容"
-            v-model="textarea"
-          >
-          </el-input></div
-      ></el-col>
-    </el-row>
+            <br />
+            <p>审核说明</p>
+            <el-input
+              type="textarea"
+              :rows="5"
+              placeholder="请输入内容"
+              v-model="textarea"
+            >
+            </el-input></div
+        ></el-col>
+      </el-row>
 
-    <div class="submit">
-      <el-button type="primary" @click="success">提交</el-button>
-    </div>
+      <div class="submit">
+        <el-button type="primary" @click="success">提交</el-button>
+      </div>
+    </el-card>
   </div>
 </template>
 
 <style scoped>
-.el-header {
-  background-color: #b3c0d1;
-  color: #333;
-  line-height: 60px;
+.el-card {
+  height: 100%;
 }
-
-.el-aside {
-  color: #333;
-}
-
 .info {
-  /* background-color: rgb(7, 7, 7);
-  background: rgba(7, 7, 7, 0.116); */
   text-align: left;
 }
 
@@ -66,11 +56,6 @@ p {
   position: relative;
   /* left: 20px; */
 }
-
-.logoImage {
-  height: 25%;
-  width: 25%;
-}
 </style>
 
 <script>
@@ -82,20 +67,18 @@ export default {
     };
   },
 
-methods: {
-
-  success() {
-        this.$alert('审核成功！', {
-          confirmButtonText: '确定',
-          callback: action => {
-            this.$message({
-              type: 'info',
-              message: `action: ${ action }`
-            });
-          }
-        });
-      }
-}
-
+  methods: {
+    success() {
+      this.$alert("审核成功！", {
+        confirmButtonText: "确定",
+        callback: (action) => {
+          this.$message({
+            type: "info",
+            message: `action: ${action}`,
+          });
+        },
+      });
+    },
+  },
 };
 </script>
