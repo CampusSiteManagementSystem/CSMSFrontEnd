5d7c9cc0267e68c1b3da106ae127986d45e4dcb6
李俊杰 <1061455426@qq.com>
2021-07-06 23:03:35 +0800

    删除了身份选择页面

--- src/page/Shared/Login.vue
+++ src/page/Shared/Login.vue
@@ -9,44 +9,76 @@
         <h1>校园场地管理系统</h1>
         <h4>Campus Site Management</h4>
       </div>
-      <div>
-        <el-form
-          ref="LoginForm"
-          :model="form"
-          :rules="rules"
-          label-width="40px"
-          :hide-required-asterisk="true"
-        >
-          <el-form-item label="账号" class="label-color" prop="accountNumber">
-            <el-input
-              v-model.number="form.accountNumber"
-              placeholder="请输入账号"
-              clearable
-            ></el-input>
-          </el-form-item>
-          <el-form-item label="密码" class="label-color" prop="password">
-            <el-input
-              v-model="form.password"
-              showPassword
-              placeholder="请输入密码"
-              clearable
-            ></el-input>
-          </el-form-item>
-          <el-row type="flex" justify="center" class="bottom-margin">
-            <router-link to="/Register" tag="el-button">注册</router-link>
-            <router-link to="/ForgetPassword" tag="el-button">忘记密码</router-link>
-            <!-- <el-button>忘记密码</el-button> -->
-            <el-button @click="submitForm('LoginForm')">登录</el-button>
-          </el-row>
-        </el-form>
-      </div>
+      <el-form
+        ref="LoginForm"
+        :model="form"
+        :rules="rules"
+        label-width="40px"
+        :hide-required-asterisk="true"
+      >
+        <el-form-item label="账号" class="label-color" prop="accountNumber">
+          <el-input
+            v-model.number="form.accountNumber"
+            placeholder="请输入账号"
+            clearable
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="密码" class="label-color" prop="password">
+          <el-input
+            v-model="form.password"
+            showPassword
+            placeholder="请输入密码"
+            clearable
+          ></el-input>
+        </el-form-item>
+      </el-form>
+
+      <el-radio-group v-model="identity" style="margin: 0px 0 5px 0px">
+        <el-row :gutter="35">
+          <el-col :span="14">
+            <el-radio :label="1">学生</el-radio>
+          </el-col>
+          <el-col :span="10">
+            <el-radio :label="2">场地管理员</el-radio>
+          </el-col>
+        </el-row>
+        <el-row :gutter="35">
+          <el-col :span="14">
+            <el-radio :label="3">组织</el-radio>
+          </el-col>
+          <el-col :span="10">
+            <el-radio :label="4">系统管理员</el-radio>
+          </el-col>
+        </el-row>
+      </el-radio-group>
+
+      <el-row :gutter="50" style="margin: 0px 0 5px 0px">
+        <el-col :span="10" :offset="2">
+          <el-button
+            @click="submitForm('LoginForm')"
+            type="primary"
+            plain
+            size="medium"
+            >登录</el-button
+          >
+        </el-col>
+        <el-col :span="12">
+          <el-button @click="forgetPassword" type="primary" plain size="medium"
+            >忘记密码</el-button
+          >
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :offset="24" :span="24">
+          <el-button plain @click="register" size="mini">注册</el-button>
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
 
 <script>
-
-
 export default {
   data() {
     return {
@@ -68,9 +100,16 @@ export default {
           },
         ],
       },
+      identity: 1,
     };
   },
   methods: {
+    forgetPassword: function () {
+      this.$router.push("/ForgetPassword");
+    },
+    register: function () {
+      this.$router.push("/Register");
+    },
     submitForm: function (formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
