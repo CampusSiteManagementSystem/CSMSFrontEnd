10fd53733e391936e5d1aa6accb2de98c6de246e
Zzy <74710014+Zhaobaba918@users.noreply.github.com>
2021-07-10 22:35:25 +0800

    所有主页OK！！！

--- src/page/Grandsman/GroundsmanHome.vue
+++ src/page/Grandsman/GroundsmanHome.vue
@@ -30,7 +30,7 @@
         <el-card class="system-announcement-card">
           <div slot="header" class="clearfix">
             <span>系统公告</span>
-            <router-link to="/Groundsman/Announcement"><el-button style="float: right; padding: 3px 0" type="text"
+            <router-link to="/GroundsAdmin/ReviewActivityList"><el-button style="float: right; padding: 3px 0" type="text"
               >查看更多</el-button
             ></router-link>
           </div>
@@ -60,7 +60,7 @@
         <el-card class="appointment-card">
           <div slot="header" class="clearfix">
             <span>待审核预约</span>
-            <router-link to="/ReviewActivityList">
+            <router-link to="/GroundsAdmin/ReviewActivityList">
             <el-button style="float: right; padding: 3px 0" type="text"
               >查看更多</el-button
             >
