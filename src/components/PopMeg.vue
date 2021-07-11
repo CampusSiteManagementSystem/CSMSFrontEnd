97ff464b7448cfa77e24beb6c35a020516e97d29
YWT Wu <wywt789@outlook.com>
2021-07-11 16:54:01 +0800

    去除popmeg的elcard

--- src/components/PopMeg.vue
+++ src/components/PopMeg.vue
@@ -1,16 +1,19 @@
 <template>
-<div>
-  <el-card class="box-card">
+  <div class="popupmeg">
+    <!-- <el-card class="box-card"> -->
       <div slot="header" class="clearfix">
-    <span>{{building}}</span>
-    <el-button @click="jump()" style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
-  </div>
-  <h3>场地总数量：{{all}}</h3>
-  <h3>空闲场地数量:{{freeRoom}}</h3>
-  <h3>场地描述：{{description}}</h3>
-</el-card>
- 
-
+        <span>{{ building }}</span>
+        <el-button
+          @click="jump()"
+          style="float: right; padding: 3px 0"
+          type="text"
+          >查看</el-button
+        >
+      </div>
+      <p>场地总数量：{{ all }}</p>
+      <p>空闲场地数量:{{ freeRoom }}</p>
+      <p>场地描述：{{ description }}</p>
+    <!-- </el-card> -->
   </div>
 </template>
 
@@ -19,22 +22,17 @@ export default {
   name: "PopMeg",
   props: {
     building: String,
-    all:Number,
-    freeRoom:Number,
-    description:String,
-    router:null,
-    membertype:Boolean,
-
-
-
+    all: Number,
+    freeRoom: Number,
+    description: String,
+    router: null,
+    membertype: Boolean,
   },
   data() {
-   return{
-
-   }
+    return {};
   },
-  methods:{
-    jump: function(){
+  methods: {
+    jump: function () {
       // jump: () => {
 
       // console.log("111111");
@@ -42,58 +40,59 @@ export default {
       console.log("111111");
       console.log(this.$props.building);
       console.log(this.$props.router);
-      
+
       console.log("111111");
-      if(this.$props.membertype)//stu
-      {
-        this.$props.router.push({  
-            name: 'StuShowPlaceDetail',  
-            params: {   
+      if (this.$props.membertype) {
+        //stu
+        this.$props.router.push({
+          name: "StuShowPlaceDetail",
+          params: {
+            key: "key",
+            msgKey: this.msg,
+          },
+          /*query: {  
                 key: 'key',   
                 msgKey: this.msg  
-            }  
-            /*query: {  
+            }*/
+        });
+      } else {
+        this.$props.router.push({
+          name: "OrgCheckSite",
+          // name: '/CheckSite',
+          params: {
+            key: "key",
+            msgKey: this.msg,
+          },
+          /*query: {  
                 key: 'key',   
                 msgKey: this.msg  
-            }*/  
-        })
-      }else{
-        this.$props.router.push({  
-            name: 'OrgCheckSite',   
-            // name: '/CheckSite',  
-            params: {   
-                key: 'key',   
-                msgKey: this.msg  
-            }  
-            /*query: {  
-                key: 'key',   
-                msgKey: this.msg  
-            }*/  
-        })
+            }*/
+        });
       }
-       
     },
-  }
+  },
 };
 </script>
 
 <style scoped>
+.text {
+  font-size: 14px;
+}
 
-  .text {
-    font-size: 14px;
-  }
+.item {
+  margin-bottom: 18px;
+}
 
-  .item {
-    margin-bottom: 18px;
-  }
-
-  .clearfix:before,
-  .clearfix:after {
-    display: table;
-    content: "";
-  }
-  .clearfix:after {
-    clear: both
-  }
-
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+.popupmeg{
+  /* backdrop-filter: blur(10px) */
+  background: transparent;
+}
 </style>
