a5a90973e4c1be80b19599a0544dcf1332e2d8cc
user3984 <63094060+user3984@users.noreply.github.com>
2021-07-11 09:23:52 +0800

    Create 404.vue

---
+++ src/page/Shared/404.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="maindiv">
+      <h1>404</h1>
+      <h2>抱歉，您要访问的页面不存在！</h2>
+</template>
+
+
+<style>
+html,
+body,
+#app,
+.el-header {
+  background-color: white;
+}
+.el-aside {
+  background-color: white;
+}
+.el-main {
+  background-color: rgb(237, 241, 245);
+  height: 100%;
+}
+</style>
