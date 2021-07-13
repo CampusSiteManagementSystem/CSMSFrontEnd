79fcbc33fe187d49ce0d34124c20f5213e60dce6
YWT Wu <wywt789@outlook.com>
2021-07-13 22:53:32 +0800

    完成组织收藏api调用

--- src/page/Organization/OrganizationFavorite.vue
+++ src/page/Organization/OrganizationFavorite.vue
@@ -1,11 +1,11 @@
 ﻿<template>
   <div>
-    <el-card>
+    <el-card v-if="!childPage">
       <div slot="header" class="clearfix">
         <el-row>
           <el-col :span="20">
             <div>
-              <h2>我的收藏</h2>
+              <h3>我的收藏</h3>
             </div>
           </el-col>
           <el-col :span="4">
@@ -19,15 +19,15 @@
         </el-row>
       </div>
 
-      <el-table :header-row-style="{height:'20px'}" :cell-style="{padding:'5px'}" :data="matchList" style="width: 100%" stripe>
+      <el-table :data="matchList" style="width: 100%" stripe>
         <el-table-column
           label="场地编号"
-          prop="groundID"
+          prop="groundId"
           width="auto"
         ></el-table-column>
         <el-table-column
           label="地点"
-          prop="position"
+          prop="groundName"
           width="auto"
         ></el-table-column>
 
@@ -35,8 +35,10 @@
           <template slot-scope="scope">
             <el-tag
               size="medium"
-              :type="scope.row.isBusy ? 'danger' : 'success'"
-              >{{ scope.row.isBusy ? "占用" : "空闲" }}</el-tag
+              :type="
+                scope.row.groundState == '空闲' ? 'successdanger' : 'danger'
+              "
+              >{{ scope.row.groundState }}</el-tag
             >
           </template>
         </el-table-column>
@@ -45,60 +47,50 @@
         </el-table-column>
 
         <el-table-column label="查看" width="auto">
-           <template slot-scope="scope">
+          <template slot-scope="scope">
             <router-link
               :to="{
                 name: 'ShowScheduleforOrg',
-                params: { groundID: scope.row.groundID },
+                params: { groundId: scope.row.groundId },
               }"
+              style="padding-right: 5px"
+              @click.native="handleEdit"
             >
-              <el-button
-                size="small"
-                @click="handleEdit(scope.$index, scope.row)"
-                round
-                >查看</el-button
-              >
+              <el-button size="small" round>查看</el-button>
             </router-link>
+            <el-button
+              @click.native.prevent="deleteRow(scope.$index, matchList)"
+              circle
+              type="danger"
+              icon="el-icon-delete"
+              size="small"
+            >
+            </el-button>
           </template>
         </el-table-column>
       </el-table>
     </el-card>
+    <keep-alive v-else>
+      <transition name="fade-transform" mode="out-in">
+        <router-view style="height: 100%"></router-view>
+      </transition>
+    </keep-alive>
   </div>
 </template>
 
 <script>
+import {
+  GETOrgFavorites,
+  //   DELETEOrgFavorites,
+   DELETEOrgFavorites,
+} from "../../API/http";
+import store from "../../state/state";
 export default {
-  created() {
-    this.matchList = this.favoriteList;
-  },
   data() {
     return {
-       favoriteList: [
-        {
-          groundID: 123,
-          isBusy: true,
-          position: "F201",
-          type: "室内",
-        },
-        {
-          groundID: 124,
-          isBusy: false,
-          position: "篮球场",
-          type: "室外",
-        },
-        {
-          groundID: 125,
-          isBusy: true,
-          position: "G201",
-          type: "室内",
-        },
-        {
-          groundID: 126,
-          isBusy: true,
-          position: "A201",
-          type: "室内",
-        },
-      ],
+      id: store.state.ID,
+      childPage: false,
+      favoriteList: "",
       matchList: [],
       toMatch: "",
     };
@@ -111,7 +103,7 @@ export default {
         this.matchList = [];
         for (var i = 0; i < this.favoriteList.length; i++) {
           if (
-            this.favoriteList[i].position.search(this.toMatch) != -1 ||
+            this.favoriteList[i].groundName.search(this.toMatch) != -1 ||
             this.favoriteList[i].type.search(this.toMatch) != -1
           ) {
             this.matchList.push(this.favoriteList[i]);
@@ -119,6 +111,54 @@ export default {
         }
       }
     },
+    handleEdit() {
+      this.childPage = !this.childPage;
+    },
+    fetchData() {
+      const that = this;
+      GETOrgFavorites({accountNumber:that.id})
+        .then((data) => {
+          that.favoriteList = data;
+          this.matchList = this.favoriteList;
+        })
+        .catch((err) => {
+          this.data = err;
+        });
+    },
+    deleteRow(index, rows) {
+      var tempRow=rows[index];
+      rows.splice(index, 1);
+      console.log(tempRow)
+      const that=this;
+
+
+
+      DELETEOrgFavorites({accountNumber:that.id,
+      groundId:tempRow.groundId
+      })
+        .then((data) => {
+          console.log("data",data);
+          this.fetchData();
+        })
+        .catch((err) => {
+          this.data = err;
+        });
+
+      
+
+
+
+
+     
+    },
+  },
+  mounted() {
+    this.fetchData();
+
+    this.childPage = this.$route.meta.title == "收藏" ? false : true;
+  },
+  updated() {
+    this.childPage = this.$route.meta.title == "收藏" ? false : true;
   },
 };
 </script>
