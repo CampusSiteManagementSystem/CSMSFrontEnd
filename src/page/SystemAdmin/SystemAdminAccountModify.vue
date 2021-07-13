c2b76d10cbb63f7cbb9f56471ed5d8298d5c873a
zchao1013 <473049712@qq.com>
2021-07-13 19:31:41 +0800

    为所有用户信息增加了表单验证与动画，new.vue是以前的系统管理员信息

--- src/page/SystemAdmin/SystemAdminAccountModify.vue
+++ src/page/SystemAdmin/SystemAdminAccountModify.vue
@@ -10,87 +10,56 @@
             <h3>用户头像</h3>
           </div>
           <div class="image">
-            <img
-              src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
-              class="pic"
-            />
+            <img src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" class="pic" />
           </div>
           <div class="modify">
-            <el-button type="primary" @click="success">更改照片</el-button>
+            <el-button type="primary">更改照片</el-button>
           </div>
         </el-col>
-        <el-col :span="12">
+        <el-col :span="8">
           <div>
             <h3>用户信息</h3>
           </div>
           <div id="content">
-            <el-form
-              ref="ruleForm"
-              :rules="rules"
-              :model="ruleForm"
-              label-width="100px"
-              :hide-required-asterisk="true"
-            >
-              <el-form-item label="账号：" prop="account">
-                <el-input v-model="ruleForm.account" :readonly="true"></el-input>
-              </el-form-item>
-              <el-form-item label="名称：" prop="name">
-                <div v-if="isSet == false">
-                  <el-input v-model="ruleForm.name" :readonly="true"></el-input>
-                </div>
-                <div v-else>
-                  <el-input v-model="ruleForm.name"></el-input>
-                </div>
-              </el-form-item>
-              <el-form-item label="负责人：" prop="user">
-                <div v-if="isSet == false">
-                  <el-input v-model="ruleForm.user" :readonly="true"></el-input>
-                </div>
-                <div v-else>
-                  <el-input v-model="ruleForm.user"></el-input>
-                </div>
-              </el-form-item>
-              <el-form-item label="信用分：" prop="credit">
-                <el-input v-model="ruleForm.credit" :readonly="true"></el-input>
-              </el-form-item>
-              <el-form-item label="联系方式：" prop="telephone">
-                <div v-if="isSet == false">
-                  <el-input
-                    v-model="ruleForm.telephone"
-                    :readonly="true"
-                  ></el-input>
-                </div>
-                <div v-else>
-                  <el-input v-model="ruleForm.telephone"></el-input>
-                </div>
-              </el-form-item>
-              <el-form-item label="详细信息：" prop="content">
-                <div v-if="isSet == false">
-                  <el-input
-                    v-model="ruleForm.content"
-                    :readonly="true"
-                  ></el-input>
-                </div>
-                <div v-else>
-                  <el-input v-model="ruleForm.content"></el-input>
-                </div>
-              </el-form-item>
-              <el-form-item label="组织状态：" prop="state">
-                <div v-if="isSet == false">
-                  <el-input v-model="ruleForm.state" :readonly="true"></el-input>
-                </div>
-                <div v-else>
-                  <el-input v-model="ruleForm.state"></el-input>
-                </div>
-              </el-form-item>
-            </el-form>
-            <div v-if="isSet == false" class="modify">
-              <el-button type="primary" @click="returnback">取消</el-button>
-              <el-button type="primary" @click="edit">编辑</el-button>
+            <transition name="fade-transform" mode="out-in">
+              <div v-if="isForm == true">
+                <el-form ref="ruleForm" :rules="rules" :model="ruleForm" label-width="100px"
+                  :hide-required-asterisk="true">
+                  <el-form-item label="账号：" prop="account">
+                    <el-input v-model="ruleForm.account" :readonly="true"></el-input>
+                  </el-form-item>
+                  <el-form-item label="名称：" prop="name">
+                    <el-input v-model="ruleForm.name" :readonly="true"></el-input>
+                  </el-form-item>
+                  <el-form-item label="邮箱：" prop="email">
+                    <div v-if="isForm == false">
+                      <el-input v-model="ruleForm.email" :readonly="true"></el-input>
+                    </div>
+                    <div v-else>
+                      <el-input v-model="ruleForm.email"></el-input>
+                    </div>
+                  </el-form-item>
+                </el-form>
+              </div>
+            </transition>
+
+            <transition name="fade-transform" mode="out-in">
+              <div v-if="isTable == true">
+                <el-table :show-header="false" :data="tableData" :cell-style="columnStyle" border
+                  style="width: 80%; margin-top: 20px">
+                  <el-table-column width="180" prop="title" label="标题">
+                  </el-table-column>
+                  <el-table-column prop="content" label="内容">
+                  </el-table-column>
+                </el-table>
+              </div>
+            </transition>
+            <div v-if="isTable == true" class="modify">
+              <el-button type="primary" @click="edit">编辑个人信息</el-button>
             </div>
-            <div v-else class="modify">
-              <el-button type="primary" @click="returnback">取消</el-button>
-              <el-button type="primary" @click="success">提交</el-button>
+            <div v-if="isForm == true" class="modify">
+              <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+              <el-button @click="returnback">取消</el-button>              
             </div>
           </div>
         </el-col>
@@ -108,7 +77,12 @@ p {
   color: rgb(0, 0, 0);
   position: relative;
 }
-
+.fade-transform-enter-active{
+  transition: all 0.3s;
+}
+.fade-transform-leave-active{
+  transition: all 0.3s;
+}
 .image {
   margin-top: -0.5cm;
 }
@@ -133,64 +107,101 @@ export default {
       ruleForm: {
         account: "123456",
         name: "软件学院",
-        user: "王某",
-        credit: "90",
-        telephone: "123456789",
-        content: "很好",
-        state: "已审核",
+        email: "www.ljj@sjk.com",
       },
 
       rules: {
-        name: [
-          { required: true, message: "请输入组织名称", trigger: "blur" },
-          { min: 3, max: 8, message: "长度在 3 到 8 个字符", trigger: "blur" },
-        ],
-        user: [
-          { required: true, message: "请输入负责人名称", trigger: "blur" },
-        ],
-        telephone: [
-          {
+        email: [{
             required: true,
-            message: "请输入联系电话",
+            message: "请输入邮箱",
+            trigger: "blur"
+          },
+          {
+            type: "email",
+            massage: "请正确输入邮箱",
             trigger: "blur",
           },
-          {
-            pattern: /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/,
-            message: "请正确输入联系电话，为11位数字",
-            trigger: "change",
-          },
         ],
-        content: [{ required: true, message: "请输入介绍", trigger: "blur" }],
-        state: [{ required: true, message: "请输入审核状态", trigger: "blur" }],
       },
-
-      isSet: false,
+      tableData: [{
+          title: "账号",
+          content: "122234",
+        },
+        {
+          title: "名称",
+          content: "数据库小组",
+        },
+        {
+          title: "邮箱",
+          content: "www.ljj@sjk.com",
+        },
+      ],
+      isForm: false,
+      isTable: true
     };
   },
-
+  mounted() {
+    this.updateData();
+  },
   methods: {
-    returnback() {
-      this.$router.push({ path: "MaintainUserInfo" });
+    columnStyle({
+      row,
+      column,
+      rowIndex,
+      columnIndex
+    }) {
+      row;
+      column;
+      //console.log(row, column, rowIndex, columnIndex, "row");
+      if (columnIndex == 0 && rowIndex < 2) {
+        return "background:#FBFBEF; font-weight: 700;";
+      } else if (columnIndex == 0) {
+        return "background:#EFFBEF; font-weight: 700;";
+      }
     },
-    // columnStyle({ row, column, rowIndex, columnIndex }) {
-    //   console.log(row, column, rowIndex, columnIndex, "row");
-    //   if (columnIndex == 0) {
-    //     return "background:#EFFBEF; font-weight: 700;";
-    //   }
-    // },
+    returnData(){
+      this.ruleForm.account = this.tableData[0].content;
+      this.ruleForm.name = this.tableData[1].content;
+      this.ruleForm.email = this.tableData[2].content;
+    },
+    updateData() {
+      this.tableData[0].content = this.ruleForm.account;
+      this.tableData[1].content = this.ruleForm.name;
+      this.tableData[2].content = this.ruleForm.email;
+    },
     edit() {
-      this.isSet = true;
+      this.isTable = false;
+      setTimeout(() => {
+        this.isForm = true;
+      }, 400);
     },
-    success() {
-      this.isSet = false;
-      this.$alert("提交成功！", {
-        confirmButtonText: "确定",
-        callback: (action) => {
-          this.$message({
-            type: "info",
-            message: `action: ${action}`,
+    returnback(){
+      this.isForm = false;
+      setTimeout(() => {
+        this.isTable = true;
+        this.returnData();
+      }, 400);
+    },    
+    submitForm: function (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.isForm = false;
+          setTimeout(() => {
+            this.isTable = true;
+            this.updateData();
+          }, 400);
+          this.$alert("编辑成功！", {
+            confirmButtonText: "确定",
+            callback: (action) => {
+              if (action === "confirm") {
+                this.$message({
+                  type: "success",
+                  message: "编辑成功",
+                });
+              }
+            },
           });
-        },
+        }
       });
     },
   },
