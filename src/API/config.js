bbefcb80e86f8c89b3a2923f74f7f2cba02cf63b
李俊杰 <1061455426@qq.com>
2021-07-14 16:59:53 +0800

    Update config.js

--- src/API/config.js
+++ src/API/config.js
@@ -25,7 +25,7 @@ axios.interceptors.request.use(
         // 每次发送请求之前判断vuex中是否存在token        
         // 如果存在，则统一在http请求的header都加上token，这样后台根据token判断你的登录情况
         // 即使本地存在token，也有可能token是过期的，所以在响应拦截器中要对返回状态进行判断 
-        const token = localStorage.getItem("accessToken")
+        const token = localStorage.getItem("uuToken")
         if (token) {
             config.headers.Authorization = 'Bearer ' + token
         }
