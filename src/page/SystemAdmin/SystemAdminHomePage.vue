fcdaf45793e4a579a80b0318dc417616028312aa
Zzy <74710014+Zhaobaba918@users.noreply.github.com>
2021-07-16 13:24:24 +0800

    添加滚动条

--- src/page/SystemAdmin/SystemAdminHomePage.vue
+++ src/page/SystemAdmin/SystemAdminHomePage.vue
@@ -207,6 +207,7 @@ export default {
     GETSystemAdministratorsID(store.state.ID)
       .then((data) => {
         this.sysInfo = data;
+        this.sysInfo.image = "https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png";
         console.log(">>>>>>", data);
       })
       .catch((err) => {
